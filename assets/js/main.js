f85917a60ecb7b4a8c56990ebf6be66ca64b54da
KhurshidShod <khurshid.lenovo@gmail.com>
2024-10-28 00:18:07 -0700

    main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,11 @@
 import "/assets/sass/vendors/base.scss";
 import "/assets/sass/main.scss";
+const popUp = document.querySelector(".article__popup_wrapper");
+document.querySelector(".close_popup").addEventListener("click", () => {
+  popUp.style.display = "none";
+});
+document.querySelectorAll(".article__tab_single").forEach((item) => {
+  item.addEventListener("click", () => {
+    popUp.style.display = "flex";
+  });
+});
